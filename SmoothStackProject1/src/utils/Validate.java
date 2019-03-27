0561b43c72d935dc8e1305e332ea860c7432cefe
vyeung <vl.yeung@yahoo.com>
2019-03-27 16:11:07 -0400

    added checkValidPhoneNum method

--- SmoothStackProject1/src/utils/Validate.java
+++ SmoothStackProject1/src/utils/Validate.java
@@ -1,15 +1,25 @@
 package utils;
 
+import java.util.InputMismatchException;
+import java.util.regex.Pattern;
+
 public class Validate {
-	public static boolean checkValidID(String input) {
+	
+	public static boolean checkValidNum(String input) {
 		try {
 			Integer.parseInt(input);
 			return true;
-		}catch(NumberFormatException e){
+		}
+		catch(InputMismatchException e) {
 			return false;
 		}
-		//input = scan.nextLine();
-		//while(!checkValidID(input);
-		//	input = scan.nextLine();
+	}
+	
+	public static boolean checkValidPhoneNum(String input) {
+		
+		String regex = "[0-9]{3}-[0-9]{3}-[0-9]{4}";
+		boolean result = Pattern.matches(regex, input);
+		
+		return result ? true : false;
 	}
 }
